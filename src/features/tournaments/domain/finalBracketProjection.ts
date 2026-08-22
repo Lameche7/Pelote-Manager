6b0d81bbe0852a84255d13e6f04edf345ee0dd2c
Lameche7 <alain.guem@gmail.com>
2026-08-22 13:30:05 +0200

    test(tournaments): rendre la projection compatible NodeNext

--- src/features/tournaments/domain/finalBracketProjection.ts
+++ src/features/tournaments/domain/finalBracketProjection.ts
@@ -1,7 +1,7 @@
 import {
   buildFinalStagePlan,
   type FinalStageSeedSource,
-} from "./finalStageEngine";
+} from "./finalStageEngine.js";
 
 export type FinalBracketProjectionSeed = {
   seed: number;
