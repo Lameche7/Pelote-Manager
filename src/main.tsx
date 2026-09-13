114b844703340515dd08991b442bde279bc3e316
Lameche7 <alain.guem@gmail.com>
2026-09-13 18:25:24 +0200

    style(forms): charger les styles de champs obligatoires

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import "./index.css";
+import "./styles/forms.css";
 import App from "@/app/App";
 import "@/features/admin/tournaments/pages/AdminTournamentPlanningCompact.css";
 import { setupTvFullscreenPrompt } from "@/features/tv/tvFullscreenPrompt";
